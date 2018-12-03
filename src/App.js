f28cc6fd33706343c7698970ea14e9d81fe65b1a
pallam01 <madusha.pallawela@bbc.co.uk>
2018-12-03 10:48:49 +0000

    Initial start of side panel.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 import './App.css';
 import AppBar from './AppBar';
 import SearchBar from './SearchBar';
-
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+// Be sure to include styles at some point, probably during your bootstraping
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 
 class App extends Component {
     render() {
@@ -10,6 +12,41 @@ class App extends Component {
             <div>
                 <AppBar variant="raised" color="primary"/>
                 <SearchBar/>
+                <SideNav
+                    onSelect={(selected) => {
+                        // Add code here
+                    }}
+                >
+                    <SideNav.Toggle />
+                    <SideNav.Nav defaultSelected="home">
+                        <NavItem eventKey="home">
+                            <NavIcon>
+                                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                            </NavIcon>
+                            <NavText>
+                                Home
+                            </NavText>
+                        </NavItem>
+                        <NavItem eventKey="charts">
+                            <NavIcon>
+                                <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+                            </NavIcon>
+                            <NavText>
+                                Charts
+                            </NavText>
+                            <NavItem eventKey="charts/linechart">
+                                <NavText>
+                                    Line Chart
+                                </NavText>
+                            </NavItem>
+                            <NavItem eventKey="charts/barchart">
+                                <NavText>
+                                    Bar Chart
+                                </NavText>
+                            </NavItem>
+                        </NavItem>
+                    </SideNav.Nav>
+                </SideNav>
             </div>
         );
     }
