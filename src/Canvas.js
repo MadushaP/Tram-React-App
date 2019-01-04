704703115ad770f5dbb69572875069ca2cfe4dae
pallam01 <madusha.pallawela@bbc.co.uk>
2019-01-04 11:21:46 +0000

    Added rotating to shape.

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
-import { Scene, FreeCamera, HemisphericLight, Sphere, Ground, Engine } from 'react-babylonjs'
-import { Vector3 } from 'babylonjs';
+import { Scene, ArcRotateCamera, EnvironmentHelper, IcoSphere, Engine, StandardMaterial, DirectionalLight, ShadowGenerator, TransformNode } from 'react-babylonjs'
+import { Vector3, Axis, Color3 } from 'babylonjs';
+import SingleAxisRotateMeshBehavior from './SingleAxisRotateMeshBehavior'
 
 class Canvas extends Component {
     render() {
         return (
             <Engine>
                 <Scene >
-                    <FreeCamera name="camera1" position={new Vector3(0, 2, -10)} target={Vector3.Zero()} />
-                    <HemisphericLight name="light1" intensity={0.7} direction={Vector3.Up()} />
-                    <Sphere name="sphere1" diameter={2} segments={16} position={new Vector3(0, 1, 0)} />
-                    <Ground name="ground1" width={6} height={6} subdivisions={2} />
+                    <ArcRotateCamera name="camera1"
+                        alpha={Math.PI / -2} beta={Math.PI / 4}
+                        radius={10} target={Vector3.Zero()} minZ={0.001} />
+                    <DirectionalLight name="dl" direction={new Vector3(0, -2.5, 0.5)} position={new Vector3(0, 2, 0.5)}>
+                        <ShadowGenerator mapSize={1024} useBlurExponentialShadowMap={true} blurKernel={32}
+                            shadowCasters={["counterClockwise", "clockwise", "BoomBox"]}
+                        />
+                    </DirectionalLight>
+                    <IcoSphere name="counterClockwise" radius={1.5} flat={true} subdivisions={1}>
+                        <StandardMaterial diffuseColor={Color3.Red()} specularColor={Color3.White()} />
+                        <SingleAxisRotateMeshBehavior rpm={5} axis={Axis.Y} />
+                    </IcoSphere>
+                    <EnvironmentHelper enableGroundShadow={true} groundYBias={1} mainColor={Color3.FromHexString("#74b9ff")} />
                 </Scene>
             </Engine>
         )
