763850e2651fab866e39d087c202a92757c57fae
pallam01 <madusha.pallawela@bbc.co.uk>
2019-02-25 10:35:24 +0000

    Color picker now picks changes color of the shape.

--- src/Canvas.js
+++ src/Canvas.js
@@ -6,11 +6,24 @@ import SingleAxisRotateMeshBehavior from './SingleAxisRotateMeshBehavior'
 class Canvas extends Component {
 
     state = {
-        switch: false
+        switch: false,
+        shapeColor: {
+            r: 199,
+            g: 0,
+            b: 57
+        }
     }
 
-    selectReflective(coordinates) {
-        this.setState({ switch: !this.state.switch })
+    changeColor(selectedColor) {
+
+        this.setState({
+            shapeColor: {
+              r: selectedColor.r * 255 ,
+              g: selectedColor.g  * 255  ,
+              b: selectedColor.b  * 255
+
+            }})
+
     }
 
 
@@ -22,11 +35,11 @@ class Canvas extends Component {
                         alpha={Math.PI / -2} beta={Math.PI / 3}
                         radius={10} target={Vector3.Zero()} minZ={0.001} />
                     <DirectionalLight name="dl" direction={new Vector3(-1, -1.5, 0.5)} position={new Vector3(2, 3, 1.5)}>
-                        <ShadowGenerator mapSize={1024} useBlurExponentialShadowMap={true} blurKernel={32}
+                        <ShadowGenerator mapSize={1024} useBlurExponentialShadowMap={true} blurKernel={16}
                             shadowCasters={["counterClockwise", "clockwise", "BoomBox"]} />
                     </DirectionalLight>
                     <IcoSphere name="counterClockwise" radius={1.5} flat={true} subdivisions={1}>
-                        <StandardMaterial diffuseColor={this.state.switch ? Color3.Red() : Color3.Yellow()} specularColor={Color3.White()} />
+                        <StandardMaterial diffuseColor={Color3.FromInts(this.state.shapeColor.r, this.state.shapeColor.g,this.state.shapeColor.b)} specularColor={Color3.White()} />
                         <SingleAxisRotateMeshBehavior rpm={5} axis={Axis.Y} />
                     </IcoSphere>
                     <GUI3DManager name="gui3d">
@@ -34,19 +47,16 @@ class Canvas extends Component {
                             <HolographicButton
                                 key={`switch`}
                                 name={`switch`}
-                                text={`Switch colour`}
-                                onPointerClickObservable={this.selectReflective.bind(this)} />
-
+                                text={`Switch colour`}/>
                         </PlanePanel>
                     </GUI3DManager>
                     <Plane position={new Vector3(0, 2, 0)}>
                         <AdvancedDynamicTexture createForParentMesh={true}>
                             <StackPanel>
-                                <ColorPicker height="1500px" width="1500px" scaleY="1.0" scaleX="1.0"></ColorPicker>
+                                <ColorPicker height="1400px" width="1400px"  onValueChangedObservable={this.changeColor.bind(this)}></ColorPicker>
                             </StackPanel>
                         </AdvancedDynamicTexture>
                     </Plane>
-
                     <EnvironmentHelper enableGroundShadow={true} groundYBias={1} mainColor={Color3.FromHexString("#74b9ff")} />
                 </Scene>
             </Engine>
